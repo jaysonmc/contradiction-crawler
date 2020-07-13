32e40ce937478892032708524cb9cfe9c8ab2df2
Jayson McIntosh <jayson.mcintosh@abydostech.com>
2020-07-13 14:30:14 -0400

    reformatting main

--- project/main.py
+++ project/main.py
@@ -7,22 +7,28 @@ from scrapy.crawler import CrawlerProcess
 
 def main():
 
-    url = sys.argv[1:]
-    if not url:
-        url = ['https://sara-sabr.github.io/ITStrategy/home.html']
-        print('Defaulting to ' + url[0])
+    def crawl(url):
+        print("Crawling %s" % url)
 
-    print("Crawling %s" % url)
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        })
 
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
+        process.crawl(
+            Crawler, start_urls=url)
+        process.start()
 
-    process.crawl(
-        Crawler, start_urls=url)
-    process.start()
+        print("Done")
 
-    print("Done")
+    def getUrl():
+        url = sys.argv[1:]
+        if not url:
+            url = ['https://sara-sabr.github.io/ITStrategy/home.html']
+            print('Defaulting to ' + url[0])
+        return url
+
+    # Run
+    crawl(getUrl())
 
 
 if __name__ == "__main__":
