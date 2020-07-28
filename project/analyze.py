efceeb8cd544950f748610c309b970b20db0024b
Jayson McIntosh <jayson.r.mcintosh@gmail.com>
2020-07-28 14:54:37 -0400

    Creating functions to scan through file

--- project/analyze.py
+++ project/analyze.py
@@ -1,7 +1,29 @@
+import settings
+from pathlib import Path
+
 class Analyze:
 
     def __init__(self):
         self.analyze()
 
     def analyze(self):
-        print("Analyzing")
+        print("Analzying")
+        pathlist = Path(settings.PROCESSED_FOLDER).rglob('*.html')
+        for path in pathlist:
+            print(path)
+            output_lines = open(path, "r").readlines() 
+            self.scanFile(output_lines)
+
+    def scanFile(self, output_lines):
+        for firstSentence in output_lines: 
+            print("firstSentence = " + firstSentence)
+            self.searchContradictions(firstSentence, output_lines)
+
+    def searchContradictions(self, firstSentence, output_lines):
+        for secondSentence in output_lines: 
+            print("Comparing ... ")
+            print(firstSentence)
+            print("With ... ")
+            print(secondSentence)
+
+            
